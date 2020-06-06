44be482b549ad040dc30a9c22e4ffe4df2940662
KK964 <morrkade05@gmail.com>
2020-06-05 19:38:54 -0500

    added fancy text

--- index.js
+++ index.js
@@ -37,16 +37,16 @@ bot.on('message', message => {
             mperson.roles.add("717631710761844757").catch(console.error)
             mperson.roles.remove("718154458131071106").catch(console.error);
 
-            message.channel.send(`${mperson.displayName} has now been muted for ${ms(ms(time))} for ${reason3}`)
-            mperson.send(`you have been muted for ${reason3} by ${message.member.displayName}`).catch(console.error);
-            bot.channels.cache.get(`717807253519990982`).send(`${mperson.displayName} has now been muted for ${ms(ms(time))} for ${reason3} by ${message.member.displayName}`)
+            message.channel.send(`***${mperson.displayName}*** has now been muted for ${ms(ms(time))} for ***${reason3}***`)
+            mperson.send(`you have been muted for ***${reason3}*** by ***${message.member.displayName}***`).catch(console.error);
+            bot.channels.cache.get(`717807253519990982`).send(`***${mperson.displayName}*** has now been muted for ${ms(ms(time))} for ***${reason3}*** by ***${message.member.displayName}***`)
             setTimeout(function(){
                 if(mperson.roles.cache.has("717631710761844757")){   
                 mperson.roles.add("718154458131071106"),
                 mperson.roles.remove("717631710761844757"),
                 console.log(muterole),
-                message.channel.send(`${mperson.displayName} has been unmuted.`)
-                bot.channels.cache.get(`717807253519990982`).send(`${mperson.displayName} has now been unmuted`)
+                message.channel.send(`***${mperson.displayName}*** is now unmuted.`)
+                bot.channels.cache.get(`717807253519990982`).send(`***${mperson.displayName}*** is now unmuted`)
                 mperson.send(`You have been unmuted`).catch(console.error);}
             }, ms(time));
 
@@ -68,12 +68,12 @@ bot.on('message', message => {
 
             if(!muterole) return message.reply("Couldn't find the mute role.")
 
-            if(mperson.roles.cache.has("718154458131071106")){return message.reply(`the user ${mperson.displayName} is not muted`)}
+            if(mperson.roles.cache.has("718154458131071106")){return message.reply(`the user ***${mperson.displayName}*** is not muted`)}
             mperson.roles.remove("717631710761844757").catch(console.error)
             mperson.roles.add("718154458131071106").catch(console.error);
-            message.channel.send(`${mperson.displayName} has now been unmuted`)
-            mperson.send(`you have been unmuted by ${message.member.displayName}`).catch(console.error);
-            bot.channels.cache.get(`717807253519990982`).send(`${mperson.displayName} was unmuted by ${message.member.displayName}`)
+            message.channel.send(`***${mperson.displayName}*** has now been unmuted`)
+            mperson.send(`you have been unmuted by ***${message.member.displayName}***`).catch(console.error);
+            bot.channels.cache.get(`717807253519990982`).send(`***${mperson.displayName}*** was unmuted by ***${message.member.displayName}***`)
         break;
     }
 
@@ -85,9 +85,9 @@ bot.on('message', message => {
             if(wperson.roles.cache.has("717462983231668255")) return message.reply("This User cannot be Warned");
             if(wperson.roles.cache.has("717547940880842753")) return message.reply("This User cannot be Warned");
             const reason = args[2];
-            message.reply(`${wperson.displayName} has now been warned for ${reason}`)
-            wperson.send(`you have been warned by ${message.member.displayName} for ${reason}`).catch(console.error);
-            bot.channels.cache.get(`717807253519990982`).send(`${wperson.displayName} was warned by ${message.member.displayName} for ${reason}`)
+            message.reply(`***${wperson.displayName}*** has now been warned for ***${reason}***`)
+            wperson.send(`you have been warned by ***${message.member.displayName}*** for ***${reason}***`).catch(console.error);
+            bot.channels.cache.get(`717807253519990982`).send(`***${wperson.displayName}*** was warned by ***${message.member.displayName}*** for ***${reason}***`)
         break;
     }
 
