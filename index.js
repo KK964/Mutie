29146c0dfae4bb99a4e450a40a97ab9caa18ee84
KK964 <morrkade05@gmail.com>
2020-06-09 18:36:35 -0500

    did somthing

--- index.js
+++ index.js
@@ -340,29 +340,29 @@ bot.on('message', async message => {
 
     //points :D
 
-    bot.on("message", message => {
+    client.on("message", message => {
         if (!message.content.startsWith(PREFIX)) return;
         if (message.author.bot) return;
-
+      
         if (!points[message.author.id]) points[message.author.id] = {
-            points: 0,
-            level: 0
+          points: 0,
+          level: 0
         };
         let userData = points[message.author.id];
         userData.points++;
-        
+      
         let curLevel = Math.floor(0.1 * Math.sqrt(userData.points));
         if (curLevel > userData.level) {
-            //lvl up!
-            userData.level = curLevel;
-            message.reply(`You"ve leveled up to level **${curLevel}**! Ain't that shwifty :P`)
+          // Level up!
+          userData.level = curLevel;
+          message.reply(`You"ve leveled up to level **${curLevel}**! Ain"t that dandy?`);
         }
-
+      
         if (message.content.startsWith(PREFIX + "level")) {
-            message.reply(`You are currently level ${userData.level}, with ${userData.points} points.`);
+          message.reply(`You are currently level ${userData.level}, with ${userData.points} points.`);
         }
         fs.writeFile("./points/points.json", JSON.stringify(points), (err) => {
-            if (err) console.error(err)
+          if (err) console.error(err)
         });
 
     })
