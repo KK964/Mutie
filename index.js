b84f3a60d4a827d8a3a0ac986e40049fc608cba8
KK964 <morrkade05@gmail.com>
2020-06-20 16:31:31 -0500

    removed some not needed code

--- index.js
+++ index.js
@@ -214,10 +214,6 @@ bot.on('message', async message => {
         }
     }
     const time = `600000`;
-    let Admin = message.guild.roles.cache.get("717547940880842753");
-    let Mod = message.guild.roles.cache.get("717147286937010176");
-    let Owner = message.guild.roles.cache.get("717462983231668255");
-    let bots = message.guild.roles.cache.get("717600664133566565")
     if(message.member.roles.cache.has("717600664133566565")) {return};
 
 
